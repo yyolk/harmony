7a011d6d201610daf36e6d970b8d92b53438f294
Joseph Chiocchi <Joe@Joseph-Chiocchis-iMac.local>
2010-08-24 12:33:41 -0400

    Added 3 new symmetrical brushes, which just build upon the ones already there.

---
+++ src/js/brushes/smirror.js
@@ -0,0 +1,51 @@
+function smirror( context )
+{
+	this.init( context );
+	SCREEN_WIDTH = window.innerWidth;
+   	SCREEN_HEIGHT = window.innerHeight;
+}
+
+smirror.prototype =
+{
+	context: null,
+
+	prevMouseX: null, prevMouseY: null,
+
+	init: function( context )
+	{
+		this.context = context;
+		this.context.globalCompositeOperation = 'source-over';
+	},
+
+	destroy: function()
+	{
+	},
+
+	strokeStart: function( mouseX, mouseY )
+	{
+		this.prevMouseX = mouseX;
+		this.prevMouseY = mouseY;
+	},
+
+	stroke: function( mouseX, mouseY )
+	{
+		this.context.lineWidth = BRUSH_SIZE;	
+		this.context.strokeStyle = "rgba(" + COLOR[0] + ", " + COLOR[1] + ", " + COLOR[2] + ", " + 0.5 * BRUSH_PRESSURE + ")";
+		
+		this.context.beginPath();
+		this.context.moveTo(this.prevMouseX, this.prevMouseY);
+		this.context.lineTo(mouseX, mouseY);
+		this.context.stroke();
+		this.context.beginPath();
+		this.context.moveTo(SCREEN_WIDTH - this.prevMouseX, this.prevMouseY);
+		this.context.lineTo(SCREEN_WIDTH - mouseX,  mouseY);
+		this.context.stroke();
+		this.prevMouseX = mouseX;
+		this.prevMouseY = mouseY;
+	},
+
+	strokeEnd: function()
+	{
+
+	}
+}
